7ef586d9f219ae2c0e076d226b553702362e9edd
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-31 12:06:12 +0500

    add joins one to many

--- 9. relationships/index.sql
+++ 9. relationships/index.sql
@@ -5,11 +5,11 @@ CREATE TABLE customers(
 
 CREATE TABLE orders(
     ord_id SERIAL PRIMARY KEY,
-    order_date DATE DEFAULT CURRENT_DATE
-    price NUMERIC NOT NULL
-    cust_id INTEGER NOT NULL
-    FOREIGN KEY (cust_id) REFERENCES customers(cust_id),
-)
+    order_date DATE DEFAULT CURRENT_DATE,
+    price NUMERIC NOT NULL,
+    cust_id INTEGER NOT NULL,
+    FOREIGN KEY (cust_id) REFERENCES customers(cust_id)
+);
 
 
 
@@ -18,14 +18,14 @@ INSERT INTO customers
 VALUES 
 ('John Doe'), 
 ('Jane Smith'), 
-('Michael Johnson');
+('Michael Johnson'),
 ('Alex');
 
 INSERT INTO orders 
-(ord_date, price, cust_id) 
+(order_date, price, cust_id) 
 VALUES 
 ('2024-01-01', 250.00, 1),
 ('2024-01-15', 300.00, 1),
 ('2024-02-01', 150.00, 2),
 ('2024-03-01', 450.00, 3),
-('2024-04-04', 500.00, 2),
+('2024-04-04', 500.00, 2);
